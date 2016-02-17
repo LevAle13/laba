f395a0a7c3d1a1747086454e2b42c7edc5dd1f03
LevAle13 <levitator@rambler.ru>
2016-02-17 18:44:05 +0200

    2016/02/17 - 18:45

--- views/errorPage.php
+++ views/errorPage.php
@@ -6,6 +6,8 @@
  * Time: 17:59
  */
 
+header("HTTP/1.0 404 Not Found");
+
 echo '<div style="font-size: 34pt;  ">Grind Error Page</div><br>';
 echo '<div style="font-size: 14pt;  ">GEP Message: ' .$this->errorMessage.'</div>';
 
