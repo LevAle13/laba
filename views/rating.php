9382504d1ce90618769db69138394f94cf2878b8
LevAle13 <levitator@rambler.ru>
2016-04-13 14:07:10 +0300

    2016/04/13 - 13:44

--- views/rating.php
+++ views/rating.php
@@ -32,7 +32,7 @@ header("Content-Type: text/html; charset=utf-8");
     <div class="container">
         <div class="row" style ="border: 1px solid blue;">
 
-            <?php include 'userInfo.php';?>
+            <?php include 'UserInfo.php';?>
 
             <div class="col-md-9 col-lg-9" style ="border: 1px solid rgba(116, 23, 187, 0.19);">
                 <div style="font-family: Tahoma; padding: 3px; font-size: 11pt; color: black; ">
